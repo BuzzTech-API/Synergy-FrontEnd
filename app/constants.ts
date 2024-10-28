42085bea5a6abfbf6c559e6ff29fb4a6be9b337f
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2024-10-28 19:40:12 -0300

    fix: troca constant de static para variavel de ambiente

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://backend:5000" // Docker users
+export const BACKEND_URL = process.env.BACKEND_URL // Docker users
 
 // export const BACKEND_URL = "http://localhost:5000" // no Docker users
